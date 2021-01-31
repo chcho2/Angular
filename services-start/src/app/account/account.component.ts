851b8c5a66870dca0f07642d4e608fc5c7cca1d2
chcho2 <chcho87@gmail.com>
2021-01-31 14:04:33 -0500

    completed active to inactive users and vice versa

---
+++ services-start/src/app/account/account.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input } from '@angular/core';
+import { LoggingService } from '../logging.service';
+import { AccountsService } from '../accounts.service';
+
+@Component({
+  selector: 'app-account',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.css'],
+  // providers: [LoggingService]
+  // if do not want same instance of AccountsService as app's but want different instances, put in providers
+  // providers: [LoggingService, AccountsService]
+})
+export class AccountComponent {
+  @Input() account: {name: string, status: string};
+  @Input() id: number;
+  // @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
+
+  constructor(private loggingService: LoggingService, 
+              private accountsService: AccountsService) {
+  }
+
+  onSetTo(status: string) {
+    // this.statusChanged.emit({id: this.id, newStatus: status});
+    this.accountsService.updateStatus(this.id, status);
+    // this.loggingService.logStatusChange(status);
+    this.accountsService.statusUpdated.emit(status);
+  }
+}
